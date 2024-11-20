51e35aad01b3c2272df1e007c7df2f39fa4a1315
caioadauri <caioadaurigomes@gmail.com>
2024-11-19 21:28:14 -0300

    finish project

--- src/FocusTimer/timer.js
+++ src/FocusTimer/timer.js
@@ -1,14 +1,35 @@
 import state from "./state.js"
 import * as el from "./elements.js"
+import { reset } from "./actions.js"
+import { kitchenTimer } from "./sounds.js"
 
 export function countdown() {
+
+  clearTimeout(state.countdownId)
+
   if (!state.isRunning) {
     return
   }
 
-  console.log('inicou')
+  let minutes = Number(el.minutes.textContent)
+  let secondes = Number(el.seconds.textContent)
 
-  setTimeout(() => countdown() , 1000)
+  secondes--
+
+  if (secondes < 0) {
+    secondes = 59
+    minutes--
+  }
+
+  if (minutes < 0) {
+    reset()
+    kitchenTimer.play()
+    return 
+  }
+
+  updateDisplay(minutes, secondes)
+
+  state.countdownId = setTimeout(() => countdown() , 1000)
 }
 
 export function updateDisplay(minutes, seconds) {
